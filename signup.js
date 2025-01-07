fdc7eb6ea71730750972d42878eee246a2416a82
fhalo07 <162355087+fhalo07@users.noreply.github.com>
2025-01-07 12:51:50 +0000

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,1 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBB7QoPCF7j5hHp37ZoXESRYDbDxmJvI18",
+    authDomain: "login-d6c0c.firebaseapp.com",
+    projectId: "login-d6c0c",
+    storageBucket: "login-d6c0c.firebasestorage.app",
+    messagingSenderId: "76381852144",
+    appId: "1:76381852144:web:c9cf87f82f8d63ccd84d35",
+    measurementId: "G-VR7K6Y9QSS"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
