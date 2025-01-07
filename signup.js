eafc82248ebcb4d97c9bb3e7c8b414db89035886
fhalo07 <162355087+fhalo07@users.noreply.github.com>
2025-01-07 18:41:57 +0000

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,5 +1,6 @@
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 
   const firebaseConfig = {
     apiKey: "AIzaSyBB7QoPCF7j5hHp37ZoXESRYDbDxmJvI18",
