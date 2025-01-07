f7e1c578cf92c705c99f18dca8804ca6672eb070
fhalo07 <162355087+fhalo07@users.noreply.github.com>
2025-01-07 17:22:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyBB7QoPCF7j5hHp37ZoXESRYDbDxmJvI18",
+    authDomain: "login-d6c0c.firebaseapp.com",
+    projectId: "login-d6c0c",
+    storageBucket: "login-d6c0c.firebasestorage.app",
+    messagingSenderId: "76381852144",
+    appId: "1:76381852144:web:c9cf87f82f8d63ccd84d35",
+    measurementId: "G-VR7K6Y9QSS"
+  };
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
