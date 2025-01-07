0a90f6e296368d205caf1337c1a15246ade1bc2b
fhalo07 <162355087+fhalo07@users.noreply.github.com>
2025-01-07 16:39:25 +0000

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,15 +1,24 @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
- import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
- import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
- 
- const firebaseConfig = {
- //YOUR COPIED FIREBASE PART SHOULD BE HERE
- //WATCH THIS VIDEO TO LEARN WHAT TO PUT HERE   https://youtu.be/_Xczf06n6x0
- };
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBB7QoPCF7j5hHp37ZoXESRYDbDxmJvI18",
+    authDomain: "login-d6c0c.firebaseapp.com",
+    projectId: "login-d6c0c",
+    storageBucket: "login-d6c0c.firebasestorage.app",
+    messagingSenderId: "76381852144",
+    appId: "1:76381852144:web:c9cf87f82f8d63ccd84d35",
+    measurementId: "G-VR7K6Y9QSS"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
  function showMessage(message, divId){
     var messageDiv=document.getElementById(divId);
