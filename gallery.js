c966ef4ff0c12db288b27adbc7732eeeb06f821f
fhalo07 <162355087+fhalo07@users.noreply.github.com>
2025-01-09 17:48:26 +0000

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -3,8 +3,8 @@ const statusCircle = document.getElementById('statusCircle');
 
 // Function to check if the stream is playing or stopped
 function checkStreamStatus() {
-  if (video.paused || video.ended || video.readyState < 3) {
-    // If the video is paused, ended, or not ready to play, set the circle to red
+  if (video.paused || video.ended) {
+    // If the video is paused or ended, set the circle to red
     statusCircle.classList.remove('green');
     statusCircle.classList.add('red');
   } else {
@@ -19,8 +19,3 @@ setInterval(checkStreamStatus, 1000);
 
 // Initial check in case the stream is already loaded and playing
 checkStreamStatus();
-
-function toggleMenu() {
-  const menu = document.querySelector('.menu');
-  menu.classList.toggle('active');
-}
