35666b0396207fbb3e88f1ce5f4cdd95c788d048
fhalo07 <162355087+fhalo07@users.noreply.github.com>
2025-01-09 18:12:19 +0000

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -19,3 +19,25 @@ const statusCircle = document.getElementById('statusCircle');
 
     // Initial check
     checkStreamStatus();
+
+const statusCircle = document.getElementById('statusCircle1');
+
+    // Function to check the stream status from localStorage
+    function checkStreamStatus() {
+      const status = localStorage.getItem('streamStatus');
+      if (status === 'active') {
+        // Stream is active
+        statusCircle.classList.remove('red');
+        statusCircle.classList.add('green');
+      } else {
+        // Stream is stopped
+        statusCircle.classList.remove('green');
+        statusCircle.classList.add('red');
+      }
+    }
+
+    // Check the stream status every 1 second
+    setInterval(checkStreamStatus, 1000);
+
+    // Initial check
+    checkStreamStatus();
